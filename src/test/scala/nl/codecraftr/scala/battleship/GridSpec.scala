20e14785b8b9d6eec2f2c066c6b7219882443fc7
Roy Straub <roy.straub@codesquad.nl>
2021-12-23 17:58:07 +0100

    Better test

--- src/test/scala/nl/codecraftr/scala/battleship/GridSpec.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/GridSpec.scala
@@ -4,17 +4,17 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class GridSpec extends AnyFlatSpec with Matchers {
-  private val grid = Grid.create()
+  private val squares = Grid.create().squares
 
   "grid" should "have a size of ten by ten squares" in {
-    grid.squares should have size 100
+    squares should have size 100
   }
 
   "columns" should "run from A to J" in {
-    grid.squares.map(_.column) shouldBe Columns.values
+    squares.map(_.column) shouldBe Columns.values
   }
 
   "rows" should "run from 1 to 10" in {
-    grid.squares.map(_.row) shouldBe Rows.values
+    squares.map(_.row) shouldBe Rows.values
   }
 }
