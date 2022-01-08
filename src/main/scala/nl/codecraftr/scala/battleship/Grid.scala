5f6c8fe4ccb2c85c2eeaea5183c0a953d99cb091
Roy Straub <roy.straub@codesquad.nl>
2022-01-08 17:42:03 +0100

    Error handling for shooting the same square twice

--- src/main/scala/nl/codecraftr/scala/battleship/Grid.scala
+++ src/main/scala/nl/codecraftr/scala/battleship/Grid.scala
@@ -1,7 +1,11 @@
 package nl.codecraftr.scala.battleship
 
 case class Grid(squares: Set[Square], shots: Set[Square] = Set()) {
-  def shot(target: Square): Grid = copy(shots = this.shots + target)
+  def shot(target: Square): Either[String, Grid] =
+    if (shots(target))
+      Left(s"Square $target was already shot")
+    else
+      Right(copy(shots = this.shots + target))
 }
 
 object Grid {
--- src/test/scala/nl/codecraftr/scala/battleship/GridSpec.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/GridSpec.scala
@@ -22,10 +22,10 @@ class GridSpec extends AnyFlatSpec with Matchers {
   }
 
   "shot" should "register squares that have been shot" in {
-    Grid.create().shot(square).shots should contain(square)
+    Grid.create().shot(square).map(_.shots).shouldBe(Right(Set(square)))
   }
 
   it should "return exception given same square is shot twice" in {
-    Grid.create().shot(square).shot(square) shouldBe Left(s"Square $square was already shot")
+    Grid.create().shot(square).flatMap(_.shot(square)) shouldBe Left(s"Square $square was already shot")
   }
 }
