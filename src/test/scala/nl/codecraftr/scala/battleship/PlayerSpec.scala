3704a3bb0eb3cb04447f33878ff41092475d4ecd
Roy Straub <roy.straub@codesquad.nl>
2022-01-08 10:43:24 +0100

    Simpler tests

--- src/test/scala/nl/codecraftr/scala/battleship/PlayerSpec.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/PlayerSpec.scala
@@ -1,22 +1,27 @@
 package nl.codecraftr.scala.battleship
 
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class PlayerSpec extends AnyFlatSpec with Matchers {
+class PlayerSpec extends AnyFlatSpec with Matchers with BeforeAndAfterEach {
+  private var player: Player = _
+
+  override def beforeEach(): Unit = {
+    player = Player.create()
+  }
+
   "player" should "not have placed ships" in {
-    val result = Player.create().ships
+    val result = player.ships
 
     result should have size 0
   }
 
   it should "have a grid on which they place ships" in {
-    Player.create().grid shouldBe Grid.create()
+    player.grid shouldBe Grid.create()
   }
 
   "arrange" should "place the ship in new player" in {
-    val player = Player.create()
-
     val result = player.arrange(Destroyer("B1", "B2"))
 
     result.ships should contain(Destroyer("B1", "B2"))
