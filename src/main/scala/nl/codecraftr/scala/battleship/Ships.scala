6aafbedaf3c29835a926d755b1601fecedcfc9f9
Roy Straub <roy.straub@codesquad.nl>
2022-01-08 11:50:51 +0100

    Submarine squares

--- src/main/scala/nl/codecraftr/scala/battleship/Ships.scala
+++ src/main/scala/nl/codecraftr/scala/battleship/Ships.scala
@@ -1,15 +1,25 @@
 package nl.codecraftr.scala.battleship
 
-sealed trait Ship
+sealed trait Ship {
+  def squares: Set[Square]
+}
 
-case object AircraftCarrier extends Ship
+case object AircraftCarrier extends Ship {
+  override def squares: Set[Square] = ???
+}
 
-case object Battleship extends Ship
+case object Battleship extends Ship {
+  override def squares: Set[Square] = ???
+}
 
-case class Submarine(square1: Square, square2: Square, square3: Square) extends Ship
+case class Submarine(square1: Square, square2: Square, square3: Square) extends Ship {
+  override def squares: Set[Square] = Set(square1, square2, square3)
+}
 
-case object Cruiser extends Ship
+case object Cruiser extends Ship {
+  override def squares: Set[Square] = ???
+}
 
 case class Destroyer(square1: Square, square2: Square) extends Ship {
-  def squares: Set[Square] = Set(square1, square2)
+  override def squares: Set[Square] = Set(square1, square2)
 }
--- src/test/scala/nl/codecraftr/scala/battleship/ShipSpec.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/ShipSpec.scala
@@ -1,17 +1,26 @@
 package nl.codecraftr.scala.battleship
 
 import nl.codecraftr.scala.battleship.Columns.B
-import nl.codecraftr.scala.battleship.Rows.{ONE, TWO}
+import nl.codecraftr.scala.battleship.Rows.{ONE, THREE, TWO}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ShipSpec extends AnyFlatSpec with Matchers {
   "Destroyer" should "take up two squares" in {
-    Destroyer(Square(ONE, B), Square(TWO, B)).squares shouldBe Set(Square(ONE, B), Square(TWO, B))
+    Destroyer(Square(ONE, B), Square(TWO, B))
+      .squares
+      .shouldBe(Set(Square(ONE, B), Square(TWO, B)))
   }
 
-  "Submarine" should "take up three squares" in {}
+  "Submarine" should "take up three squares" in {
+    Submarine(Square(ONE, B), Square(TWO, B), Square(THREE, B))
+      .squares
+      .shouldBe(Set(Square(ONE, B), Square(TWO, B), Square(THREE, B)))
+  }
+
   "Cruiser" should "take up three squares" in {}
+
   "Battleship" should "take up four squares" in {}
+
   "AircraftCarrier" should "take up five squares" in {}
 }
