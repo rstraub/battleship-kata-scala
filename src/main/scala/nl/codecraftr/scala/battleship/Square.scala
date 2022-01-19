07b5876d79efc233ffebb435008d00a3ffd56813
Roy Straub <roy.straub@codesquad.nl>
2022-01-19 18:51:04 +0100

    Duplicate columns

--- src/main/scala/nl/codecraftr/scala/battleship/Square.scala
+++ src/main/scala/nl/codecraftr/scala/battleship/Square.scala
@@ -3,12 +3,6 @@ package nl.codecraftr.scala.battleship
 import nl.codecraftr.scala.battleship.Columns.Column
 import nl.codecraftr.scala.battleship.Rows.Row
 
-object Columns extends Enumeration {
-  type Column = Value
-
-  val A, B, C, D, E, F, G, H, I, J = Value
-}
-
 case class Square(column: Column, row: Row) {
   override def toString: String = s"$column${row.id}"
 }
