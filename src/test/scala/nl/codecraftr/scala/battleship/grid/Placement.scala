5b3c1d024a2b39c03cc284526d298450909c45c3
Roy Straub <roy.straub@codesquad.nl>
2022-01-19 22:20:08 +0100

    Exception when too few arguments are passed

--- src/test/scala/nl/codecraftr/scala/battleship/grid/Placement.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/grid/Placement.scala
@@ -2,4 +2,8 @@ package nl.codecraftr.scala.battleship.grid
 
 import nl.codecraftr.scala.battleship.Ship
 
-case class Placement(ship: Ship, coordinates: Set[Coordinate]) {}
+case class Placement(ship: Ship, coordinates: Set[Coordinate]) {
+  private def isValid = coordinates.size == ship.occupies
+
+  if (!isValid) throw new IllegalArgumentException("Invalid coordinates")
+}
--- src/test/scala/nl/codecraftr/scala/battleship/grid/PlacementSpec.scala
+++ src/test/scala/nl/codecraftr/scala/battleship/grid/PlacementSpec.scala
@@ -1,7 +1,7 @@
 package nl.codecraftr.scala.battleship.grid
 
 import nl.codecraftr.scala.battleship.Ship.destroyer
-import nl.codecraftr.scala.battleship.grid.Columns.{A, B}
+import nl.codecraftr.scala.battleship.grid.Columns.{A, B, C}
 import nl.codecraftr.scala.battleship.grid.Rows.{ONE, TWO}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -15,7 +15,14 @@ class PlacementSpec extends AnyFlatSpec with Matchers {
     result.coordinates shouldBe coordinates
   }
 
-  it should "not be created given too many coordinates" in {}
+  it should "not be created given too many coordinates" in {
+    val coordinates: Set[Coordinate] =
+      Set(Target(A, ONE), Target(B, TWO), Target(C, TWO))
+
+    an[IllegalArgumentException] shouldBe thrownBy(
+      Placement(destroyer(), coordinates)
+    )
+  }
 
   it should "not be created given too few coordinates" in {}
 
