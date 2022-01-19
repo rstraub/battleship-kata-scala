533743cdb3ea50b59f82ae7dc4748bfd78cdf48c
Roy Straub <roy.straub@codesquad.nl>
2022-01-19 22:09:02 +0100

    Just dumping my thoughts

---
+++ src/test/scala/nl/codecraftr/scala/battleship/grid/PlacementSpec.scala
@@ -0,0 +1,14 @@
+package nl.codecraftr.scala.battleship.grid
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class PlacementSpec extends AnyFlatSpec with Matchers {
+  it should "be created given valid coordinates" in {}
+
+  it should "not be created given too many coordinates" in {}
+
+  it should "not be created given too few coordinates" in {}
+
+  it should "not be created given coordinates are not adjacent" in {}
+}
