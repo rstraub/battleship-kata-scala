876fc79ce0abb5a19622ea437197c2bc711caa83
Roy Straub <roy.straub@codesquad.nl>
2022-01-19 18:29:49 +0100

    Extracted rows

---
+++ src/main/scala/nl/codecraftr/scala/battleship/Rows.scala
@@ -0,0 +1,16 @@
+package nl.codecraftr.scala.battleship
+
+object Rows extends Enumeration {
+  type Row = Value
+
+  val ONE = Value(1)
+  val TWO = Value(2)
+  val THREE = Value(3)
+  val FOUR = Value(4)
+  val FIVE = Value(5)
+  val SIX = Value(6)
+  val SEVEN = Value(7)
+  val EIGHT = Value(8)
+  val NINE = Value(9)
+  val TEN = Value(10)
+}
--- src/main/scala/nl/codecraftr/scala/battleship/Square.scala
+++ src/main/scala/nl/codecraftr/scala/battleship/Square.scala
@@ -9,21 +9,6 @@ object Columns extends Enumeration {
   val A, B, C, D, E, F, G, H, I, J = Value
 }
 
-object Rows extends Enumeration {
-  type Row = Value
-
-  val ONE = Value(1)
-  val TWO = Value(2)
-  val THREE = Value(3)
-  val FOUR = Value(4)
-  val FIVE = Value(5)
-  val SIX = Value(6)
-  val SEVEN = Value(7)
-  val EIGHT = Value(8)
-  val NINE = Value(9)
-  val TEN = Value(10)
-}
-
 case class Square(column: Column, row: Row) {
   override def toString: String = s"$column${row.id}"
 }
